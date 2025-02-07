7e613a29365fb383b6a6eb4c92d97a1ff63b0057
Pedro <ppineiroordax@danielcastelao.org>
2025-02-07 19:28:18 +0100

    getter y setter id añadido

--- src/main/java/com/pedro/ProyectoTFC/entities/Album.java
+++ src/main/java/com/pedro/ProyectoTFC/entities/Album.java
@@ -51,6 +51,14 @@ public class Album {
         this.imageUrl = imageUrl;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
--- src/main/java/com/pedro/ProyectoTFC/entities/Movie.java
+++ src/main/java/com/pedro/ProyectoTFC/entities/Movie.java
@@ -45,6 +45,14 @@ public class Movie {
         this.imageUrl = imageUrl;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
