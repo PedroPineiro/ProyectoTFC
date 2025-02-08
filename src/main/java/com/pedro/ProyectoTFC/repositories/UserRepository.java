412e5e75461585aeecdec143be5131cb94cd4abe
Pedro <ppineiroordax@danielcastelao.org>
2025-02-08 17:04:11 +0100

    UserRepository creado y modificado

---
+++ src/main/java/com/pedro/ProyectoTFC/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.pedro.ProyectoTFC.repositories;
+
+import com.pedro.ProyectoTFC.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
