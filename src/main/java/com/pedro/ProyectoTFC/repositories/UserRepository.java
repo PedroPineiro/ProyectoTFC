c5c5eeaf596e1213daa10098d008fa72fc22c898
Pedro <ppineiroordax@danielcastelao.org>
2025-02-08 17:20:27 +0100

    repository user implementado

--- src/main/java/com/pedro/ProyectoTFC/repositories/UserRepository.java
+++ src/main/java/com/pedro/ProyectoTFC/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.pedro.ProyectoTFC.repositories;
 
 import com.pedro.ProyectoTFC.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(@Email @NotBlank String email);
 }
