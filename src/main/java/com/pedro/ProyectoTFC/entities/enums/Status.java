b9e4c0bcbd04264021ea7fc47047bdb7741842a7
Pedro <ppineiroordax@danielcastelao.org>
2025-02-11 20:19:10 +0100

    status actualizado

--- src/main/java/com/pedro/ProyectoTFC/entities/enums/Status.java
+++ src/main/java/com/pedro/ProyectoTFC/entities/enums/Status.java
@@ -3,5 +3,4 @@ package com.pedro.ProyectoTFC.entities.enums;
 public enum Status {
     PLAYED,   // Para películas vistas
     WISHLIST,  // Para películas por ver
-    FAVORITE   // Para películas favoritas (opcional)
 }
