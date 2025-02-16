12a54e40a711d06db12800bf04c396307b16e07a
Pedro <ppineiroordax@danielcastelao.org>
2025-02-16 21:09:16 +0100

    SecurityConfig modificado para encriptar password

--- src/main/java/com/pedro/ProyectoTFC/config/SecurityConfig.java
+++ src/main/java/com/pedro/ProyectoTFC/config/SecurityConfig.java
@@ -4,17 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)  // Desactiva CSRF (solo para desarrollo)
+                .csrf(AbstractHttpConfigurer::disable) // Deshabilita CSRF
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Permite acceso sin autenticación
-                );
+                        .requestMatchers("/api/users/**").permitAll() // Permite sin autenticación
+                        .anyRequest().authenticated()  // Requiere autenticación para el resto
+                )
+                .httpBasic(httpBasic -> {}); // Usa autenticación básica
 
         return http.build();
     }
